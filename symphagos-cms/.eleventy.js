f40e19fea064343f6cb90dba939db078cd4779e9
Symphagos <symphagos@gmail.com>
2026-01-16 20:25:29 -0500

    Update .eleventy.js

--- symphagos-cms/.eleventy.js
+++ symphagos-cms/.eleventy.js
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
 
+  eleventyConfig.addFilter("padStart", function(value, length, char) {
+    return String(value).padStart(length, char || '0');
+  });
+
   eleventyConfig.addCollection("specimens", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/specimens/*.md").sort((a, b) => {
       return a.data.specimenNumber - b.data.specimenNumber;
